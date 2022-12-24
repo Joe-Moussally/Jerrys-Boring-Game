a6c086b8be077fd837f3b4c9d9af7b2c60b21323
MJoe111 <joemoussally111@gmail.com>
2022-12-24 20:41:25 +0200

    Creating sqlite queries

--- App.js
+++ App.js
@@ -6,16 +6,89 @@ import GameScreen from './screens/GameScreen';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
+//SQLite import
+import * as SQLite from "expo-sqlite";
+import { useEffect, useState } from 'react';
+
+
+
+//defining the local DB
+const db = SQLite.openDatabase("main.db")
+
+// //function to create table if it doesn't exist
+// const createTable = () => {
+//   db.transaction(tx => {
+//     tx.executeSql(
+//       "CREATE TABLE IF NOT EXIST"
+//       +"user"
+//       +"(name TEXT, balloons INTEGER)"
+//     )
+//   })
+// }
+
+// //function to set data to db
+// const setData = () => {
+//   db.transaction(tx => {
+//     tx.executeSql(
+//       "INSERT INTO user (name, balloons) VALUES ('Joe', 2000)"
+//     )
+//   })
+// }
+
+// const getData = () => {
+//   db.transaction(tx => {
+//     tx.executeSql(
+//       "SELECT name, balloons FROM user",
+//       [],
+//       (tx,result) => {
+//         console.log(result.rows)
+//         var len = result.rows.length
+//         if(len > 0) {
+//           console.log(result)
+//           var name = result.rows.item[0].name
+//           var balloons = result.rows.item[0].balloons
+//         }
+//       }
+//     )
+//   })
+// }
+
 export default function App() {
 
-  console.log()
+  const [isLoading,setIsLoading] = useState(true)
+
+  useEffect(() => {
+
+    //if new user -> create the new local db
+    // createTable()
+    // setData()
+    // getData()
+
+    db.transaction(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS user (name TEXT, balloons_count INTEGER)')
+    })
+
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO user (name, balloons_count) VALUES ("JOE",1000)')
+    })
+
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM user',null,
+        (txObj, res) => {
+          console.log(res.rows)
+        }
+      )
+    })
+
+  },[])
+
   return (
     <Provider store={store}>
       <View
-        style={{paddingTop:StatusBar.currentHeight, flex:1}}
+        style={{marginTop:StatusBar.currentHeight, flex:1}}
       >
 
-        <StatusBar translucent={true}/>
+        {/* <StatusBar translucent={true}/> */}
 
         {/* Gameplay Component */}
         <GameScreen />
